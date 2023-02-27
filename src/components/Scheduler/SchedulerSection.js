9a953b1b6954e5db65582b0ef1e78692c4e49534
화해방 <bcl0206@naver.com>
2023-02-27 23:38:27 +0900

    comeback

--- src/components/Scheduler/SchedulerSection.js
+++ src/components/Scheduler/SchedulerSection.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
+import Calendar from "./Calendar";
 
 const SchedulerSection = () => {
   return (
-    <div>
-      스케줄 페이지
-    </div>
+    <Box>
+      <Calendar />
+    </Box>
   );
 };
 
